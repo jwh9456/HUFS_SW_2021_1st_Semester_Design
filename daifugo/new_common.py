3ca0b82b0d689a1701b00544074180c253d14cb5
xieun99 <71272542+xieun99@users.noreply.github.com>
2021-04-16 15:14:00 +0900

    Update new_common.py

--- daifugo/new_common.py
+++ daifugo/new_common.py
@@ -99,51 +99,6 @@ def straights_joker(cards):  #cards는 각각의 순열들
                return False
        return True
 
-def straightsJoker(cards):  # player가 조커를 가지고 있을 경우 straigth 가능한 수 알려주는 함수
-    cards = sorted(cards, key=card_value)  # 카드 value 순으로 오름차순 정렬
-    print(cards)
-    num_cards = len(cards)
-    retval = []
-
-    if num_cards < 3:
-        return retval
-
-    for i in range(num_cards - 2):
-        if card_value(cards[i + 2]) - card_value(cards[i]) + 1 == 3 and card_value(
-                cards[i + 2]) != 13:  # 조커 있지만 스트레이트인 경우
-            retval.append(cards[i:i + 3])
-        if card_value(cards[i + 1]) - card_value(cards[i]) + 1 == 3:  # 바로 옆이 2단계 위인 경우 조커 가운데로
-            straight = []
-            straight.append(cards[i])
-            straight.append(cards[-1])
-            straight.append(cards[i + 1])
-            retval.append(straight)
-        if card_value(cards[i + 1]) - card_value(cards[i]) + 1 == 2:  # 바로 옆이 한단계 위인 경우 조커가 양 옆에 올 수 있음
-            if card_value(cards[i]) == 0:
-                straight = []
-                straight.append(cards[i])
-                straight.append(cards[i + 1])
-                straight.append(cards[-1])
-                retval.append(straight)
-            elif card_value(cards[i + 1]) == 12:
-                straight = []
-                straight.append(cards[-1])
-                straight.append(cards[i])
-                straight.append(cards[i + 1])
-                retval.append(straight)
-            else:
-                straight = []
-                straight.append(cards[-1])
-                straight.append(cards[i])
-                straight.append(cards[i + 1])
-                retval.append(straight)
-                straight = []
-                straight.append(cards[-1])
-                straight.append(cards[i])
-                straight.append(cards[i + 1])
-                retval.append(straight)
-    return retval
-
 def generate_plays(hand):                   #내가 낼 수 있는 경우의 수를 plays에 넣어서 return
    """
    Generate all possible plays from a given hand.
@@ -214,7 +169,7 @@ def is_valid_play(prev, play, debug=False):
                     return False
                 else:
                     return True
-                
+
             if debug: print("INVALID: {0} not worth more".format(play))
             return False
         
