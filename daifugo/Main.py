128715b79b79575dfe8a1f63dbbd774d9c247742
xieun99 <71272542+xieun99@users.noreply.github.com>
2021-04-30 16:22:23 +0900

    Update Main.py

--- daifugo/Main.py
+++ daifugo/Main.py
@@ -1,28 +1,64 @@
-import game as g
-import common as c
-import interactive as playerInstance
+import game
 
+# 대부호 > 부호 > 빈민 > 평민
+class _player:
+    def __init__(self, 이름, 계급, 점수):
+        self.이름 = 이름
+        self.계급 = 계급
+        self.점수 = 점수
 
-print("######## 대부호 카드 게임 시작 ########")
+    def set_score(self, winner):
+        if winner[0].이름 == self.이름:
+            self.점수 += 5
+        elif winner[1].이름 == self.이름:
+            self.점수 += 3
+        elif winner[2].이름 == self.이름:
+            self.점수 += 1
+        elif winner[3].이름 == self.이름:
+            self.점수 += 0
 
-while 1 :
-    print("게임을 시작합니다.")
-    print(g.get_deck())
-    print("패를 생성합니다.")
-    deal = g.deal()
-    g.play_game(4)
-    # print(deal)
-    # i = 1
-    # for hands in deal :
-    #     print("player {0}의 핸드 손패 : {1}".format(i,hands))
-    #     if '3D' in hands : print("player {0}가 먼저 시작합니다.".format(i))
-    #     i += 1
-    break;
+    def set_rank(self, winner):
+        if winner[0].이름 == self.이름:
+            self.계급 = "대부호"
+        elif winner[1].이름 == self.이름:
+            self.계급 = "부호"
+        elif winner[2].이름 == self.이름:
+            self.계급 = "평민"
+        elif winner[3].이름 == self.이름:
+            self.계급 = "빈민"
 
+A = _player('A', '평민', 0)
+B = _player('B', '평민', 0)
+C = _player('C', '평민', 0)
+D = _player('D', '평민', 0)
 
+players = (A, B, C, D)
+max = 0
+round = 1
 
+while (max < 18):
+    print("::::::::::대부호 게임 시작::::::::::\n")
+    print(round, "round를 시작하겠습니다\n")
+    result = game.play_game(players)
 
-# testset=['3H','4H','5H','7H','8H','9C','8C','2H','BB']
+    print("게임이 종료되었습니다.\n")
+    print("\n----- 게임의 결과 -----\n")
 
-# print(g.deal(4))
-# print(c.generate_plays(testset))
+    for i in range(4):
+        _player.set_score(players[i], result)
+        _player.set_rank(players[i], result)
+
+    print("{0}의 점수 = {1}, {2}의 계급 = {3}".format('A', A.점수, 'A', A.계급))
+    print("{0}의 점수 = {1}, {2}의 계급 = {3}".format('B', B.점수, 'B', B.계급))
+    print("{0}의 점수 = {1}, {2}의 계급 = {3}".format('C', C.점수, 'C', C.계급))
+    print("{0}의 점수 = {1}, {2}의 계급 = {3}".format('D', D.점수, 'D', D.계급))
+    print()
+
+    for i in range(4):
+        if players[i].점수 >= max:
+            max = players[i].점수
+
+    print("최대 값:{0}\n".format(max))
+    round += 1
+
+print("   대부호 게임 끝   ")
