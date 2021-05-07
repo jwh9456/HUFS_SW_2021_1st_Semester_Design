8835594a3af710551d0c410f080d1af8a7da46f7
xieun99 <71272542+xieun99@users.noreply.github.com>
2021-05-07 16:20:18 +0900

    random

--- daifugo/interactive.py
+++ daifugo/interactive.py
@@ -1,10 +1,11 @@
 """
 Interactive player.
 """
-DEBUG = True
+DEBUG = False
 import sys
 from collections import defaultdict
 import new_common as common
+import random
 
 def play(prev, hand, discard, holding, valid=common.get_valid_plays, generate=common.generate_plays, is_valid=common.is_valid_play):
   if prev is None:
@@ -35,23 +36,25 @@ def play(prev, hand, discard, holding, valid=common.get_valid_plays, generate=co
           discard_summary[card[1]].append(card)
 
   if discard_summary:
-    if DEBUG:print ("    Discard Summary:")
+    if DEBUG: print ("    Discard Summary:")
     for suit in sorted(discard_summary):
       cards = sorted(discard_summary[suit], key=common.card_value)
       ranks = [c[0] for c in cards]
       disp = ''.join( c if c in ranks else ' ' for c in common.RANKS)
       if DEBUG:print ("      {0}: {1}".format(suit, disp))
 
-  if DEBUG:print ("    Hand: {0}".format(sorted(hand, key=common.card_value)))
+  if DEBUG: print ("    Hand: {0}".format(sorted(hand, key=common.card_value)))
   if prev is not None:
-    if DEBUG:print ("    Prev: {0}".format(sorted(prev, key=common.card_value)))
+    if DEBUG: print ("    Prev: {0}".format(sorted(prev, key=common.card_value)))
   #if DEBUG:print ("    [0] Pass")
   for i, play in enumerate(new_plays):
-    if DEBUG:print ("    [{0}] {1}".format(i+1, play))
+    if DEBUG: print ("    [{0}] {1}".format(i+1, play))
 
   while True:
     try:
-      c = int(input("    CHOICE: "))
+      start = 0
+      end = len(plays) + 1
+      c = random.randint(start, end)
       if c == 0:
         return None
       elif str(c) == 'exit':
